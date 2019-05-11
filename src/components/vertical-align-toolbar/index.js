ffd18976a587f213b500bc2c1be3d6b008b0d462
Logan Stellway <loganstellway@Logans-MacBook-Pro.local>
2019-05-11 00:39:38 -0700

    Adds vertical align toolbar component

---
+++ src/components/vertical-align-toolbar/index.js
@@ -0,0 +1,55 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { Toolbar } from '@wordpress/components';
+import { withState } from '@wordpress/compose';
+
+const DEFAULT_OPTIONS = [
+    { value: '', title: 'Default' },
+    { value: 'start', title: 'Start' },
+    { value: 'end', title: 'End' },
+    { value: 'center', title: 'Center' },
+    { value: 'baseline', title: 'Baseline' },
+    { value: 'stretch', title: 'Stretch' },
+];
+
+const ColumnIcons = {
+    xs: 'smartphone',
+    sm: 'image-rotate-right',
+    md: 'tablet',
+    lg: 'laptop',
+    xl: 'desktop',
+    all: 'welcome-view-site',
+};
+
+const ColumnLabels = {
+    xs: __( 'Smart Phone' ),
+    sm: __( 'Landscape Smart Phone' ),
+    md: __( 'Tablet' ),
+    lg: __( 'Laptop' ),
+    xl: __( 'Desktop' ),
+    all: __( 'All Breakpoints' ),
+};
+
+export default withState( {
+    value: '',
+} )( ( { icon = false, breakpoint, isCollapsed = true, value, onChange, dirOptions = DEFAULT_OPTIONS } ) => {
+    return (
+        <Toolbar
+            isCollapsed={ isCollapsed }
+            icon={ icon ? icon : ColumnIcons[breakpoint] }
+            label={ ColumnLabels[breakpoint] }
+            controls={ dirOptions.map( ( control ) => {
+                const { value } = control;
+                return {
+                    ...control,
+                    isActive: value === value,
+                    onClick: () => {
+                        onChange( value );
+                    },
+                };
+            } ) }
+        />
+    );
+} );
